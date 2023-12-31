0972d36a82b4df15ea3b2939a5c243ef7610171d
Gregory Travis <gregorytravis711@gmail.com>
2023-12-31 14:25:08 -0500

    Fixed range again

--- generate.ts
+++ generate.ts
@@ -3,7 +3,7 @@ const sudoku = require('./sudoku.js');
 
 // Generates 2000 puzzles with each number of givens 42-61 inclusive (40000 total)
 for (let i = 42; i <= 61; i++) {
-    for (let j = 0; j < 40; j++) {
+    for (let j = 0; j < 2000; j++) {
         console.log(sudoku.generate(i));
     }
 }
