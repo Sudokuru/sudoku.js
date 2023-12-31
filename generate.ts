2823b1366d3cd1173a816f7a5c3898a53e1b9eee
Gregory Travis <gregorytravis711@gmail.com>
2023-12-31 13:56:51 -0500

    Fixed off by one error in comment

--- generate.ts
+++ generate.ts
@@ -1,7 +1,7 @@
 // @ts-nocheck
 const sudoku = require('./sudoku.js');
 
-// Generates 400 puzzles with each number of givens 31-61 inclusive (12000 total)
+// Generates 400 puzzles with each number of givens 31-61 inclusive (12400 total)
 for (let i = 31; i <= 61; i++) {
     for (let j = 0; j < 400; j++) {
         console.log(sudoku.generate(i));
